44f52e44678b8c22fd62952dbea5c7dcd156b0b4
gkucmierz <gkucmierz@gmail.com>
2017-08-19 15:45:52 +0200

    Regular Expression - Check if divisible by 0b101 (5)

---
+++ regular-expression-check-if-divisible-by-0b101-5/index.js
@@ -0,0 +1,4 @@
+// Regular Expression - Check if divisible by 0b101 (5)
+// https://www.codewars.com/kata/regular-expression-check-if-divisible-by-0b101-5/train/javascript
+
+const solution = /^(?=.)(0|1(10)*(0|11)(01*01|01*00(10)*(0|11))*1)*$/;
