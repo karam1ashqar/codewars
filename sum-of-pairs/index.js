37b05bf1735ef9826b5db8d32835077799ba21ce
Grzegorz Kucmierz <gkucmierz@gmail.com>
2019-11-19 15:04:52 +0100

    Sum of Pairs

---
+++ sum-of-pairs/index.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/sum-of-pairs/train/javascript
+
+const sum_pairs = (arr, sum) => {
+  const map = new WeakMap();
+  for (let i = 0; i < arr.length; ++i) {
+    if (map[arr[i]]) {
+      return [sum - arr[i], arr[i]];
+    }
+    map[sum - arr[i]] = true;
+  }
+  return [];
+};
