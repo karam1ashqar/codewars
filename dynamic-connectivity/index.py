c7474f33a4754c6bbfc60441369914b5b0109bdd
Grzegorz Kucmierz <gkucmierz@gmail.com>
2019-12-18 15:35:04 +0100

    Dynamic Connectivity

---
+++ dynamic-connectivity/index.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/dynamic-connectivity/python
+
+class DynamicConnectivity(object):
+    def __init__(self, n):
+        self.__map = list(range(0, n))
+    
+    def __root(self, p):
+        while p != self.__map[p]:
+            self.__map[p] = self.__map[self.__map[p]]
+            p = self.__map[p]
+        return p
+    
+    def union(self, p, q):
+        self.__map[self.__root(p)] = self.__root(q)
+        
+    def connected(self, p, q):
+        return self.__root(p) == self.__root(q)
